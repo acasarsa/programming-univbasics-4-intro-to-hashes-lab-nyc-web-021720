96b33968200bd7ae3714ba05ad2c87b49711eb62
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-01 18:16:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 def my_hash_creator(key, value)
   key = :item
   value = "price.to_int"
-  {}
+  {key: value}
   # return a hash that includes the key and value parameters passed into this method
 end
 

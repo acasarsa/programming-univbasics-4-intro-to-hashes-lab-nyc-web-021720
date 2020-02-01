4b250e48f560b0f6ff380cf98135e09a235e4c1c
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-01 19:41:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def my_hash_creator(key, value)
   new_hash[key] = value
   new_hash
 end
-# so useing :key would create a new key to add but we want to just use the key that already exists. so we assign the arguement [key] to new_hash just by 
+# so useing :key would create a new key to add but we want to just use the key that already exists. so we assign the arguement [key] to new_hash just by
 # new_hash[key] then set it to equal the agument of value
 # return a hash that includes the key and value parameters passed into this method
 
@@ -42,7 +42,7 @@ end
 
 # not working
 def update_counting_hash(hash, key)
-  
+
   if hash[key]
     hash[key] += 1
 
